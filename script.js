9429b21671cf9e9eedb0840d9015bae1cce05b9c
Niédja Bezerra <niedjacarlos@gmail.com>
2022-01-24 03:23:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//1 - crie uma função que exiba uma mensagem no console
+function greeting() {
+    console.log("Hi!Hallo!Holá!");
+}
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function name() {
+    console.log('Hi. My name is ${name}!');
+}
+//3 - crie uma função que receba seu nome, idade, e estilo musical preferido (parâmetros) e exiba no console
+function intro(name, age, musicStyle) {
+    console.log(`Olá! Me chamo ${name}, tenho ${age} anos e adoro ${musicStyle}`);
+}
+//4 - crie uma função que receba o seu filme favorito, musica favorita (parâmetros) e exiba no console
+function curiosities (movie, song) {
+    console.log(`Meu filme favorito é ${movie} e minha música favorita é ${song}.');
+}
+
